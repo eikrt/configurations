a71899cd8717c87ff8e4fb7fadbf1bf87fd3674b
Eino Korte <e.i.korte@gmail.com>
2021-08-31 15:38:51 +0300

    added script

--- desktop/dwm/dwm.c
+++ desktop/dwm/dwm.c
@@ -44,7 +44,7 @@
 
 #include "drw.h"
 #include "util.h"
-
+#include "../repo/configurations/programs/lemonsocket/client/lemonclient.h"
 /* macros */
 #define BUTTONMASK              (ButtonPressMask|ButtonReleaseMask)
 #define CLEANMASK(mask)         (mask & ~(numlockmask|LockMask) & (ShiftMask|ControlMask|Mod1Mask|Mod2Mask|Mod3Mask|Mod4Mask|Mod5Mask))
@@ -2056,16 +2056,7 @@ updatewmhints(Client *c)
 void
 view(const Arg *arg)
 {
-
-
-	FILE *fptr;
-	fptr = fopen("/tmp/tag", "w");
-	if (fptr == NULL) {
-		printf("Error opening /tmp/tag");
-		exit(1);
-	}
-	fprintf(fptr, "%i", (int) arg->ui);
-       	fclose(fptr);
+	sendTag(arg->ui);
 	if ((arg->ui & TAGMASK) == selmon->tagset[selmon->seltags])
 		return;
 	selmon->seltags ^= 1; /* toggle sel tagset */
